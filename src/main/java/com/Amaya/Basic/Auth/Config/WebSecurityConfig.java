120847d50bc536222a41e4322ded799587830b41
Amayap14 <juan.amaya.p14@gmail.com>
2023-09-27 12:16:22 -0500

    basic auth

---
+++ src/main/java/com/Amaya/Basic/Auth/Config/WebSecurityConfig.java
@@ -0,0 +1,52 @@
+package com.Amaya.Basic.Auth.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers(HttpMethod.GET, "/").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/user").hasRole("USER")
+                        .requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("amaya")
+                .password("12345")
+                .roles("ADMIN")
+                .build();
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("perez")
+                .password("12345")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, user1);
+    }
+}
---
+++ src/main/java/com/Amaya/Basic/Auth/Controller/PrincipalController.java
@@ -0,0 +1,33 @@
+package com.Amaya.Basic.Auth.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PrincipalController {
+
+
+    @GetMapping("/")
+    public String publico(){
+
+        return "Hola mundo";
+
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+
+        return "Hola admin";
+
+    }
+
+    @GetMapping("/user")
+    public String user(){
+
+        return "Hola user";
+
+    }
+
+}
